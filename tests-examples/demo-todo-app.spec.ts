bbf57a59ec21f4c2f95a75e290fc5bc4b0410eb0
PrzemysławPacyk <przemyslaw.pacyk@execon.pl>
2025-09-03 11:50:01 +0200

    chore: eslint config

--- tests-examples/demo-todo-app.spec.ts
+++ tests-examples/demo-todo-app.spec.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { test, expect, type Page } from '@playwright/test';
 
 test.beforeEach(async ({ page }) => {
@@ -483,6 +484,7 @@ async function checkNumberOfCompletedTodosInLocalStorage(
 async function checkTodosInLocalStorage(page: Page, title: string) {
   return await page.waitForFunction((t) => {
     return JSON.parse(localStorage['react-todos'])
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
       .map((todo: any) => todo.title)
       .includes(t);
   }, title);
