4483ba37483800705da172aead55c100cb48414f
PrzemysławPacyk <przemyslaw.pacyk@execon.pl>
2025-10-01 14:21:43 +0200

    chore: test data for user mail

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,12 @@
 import { LoginPage } from '../src/pages/login.page';
 import { WelcomePage } from '../src/pages/welcome.page';
+import { testUser1 } from '../src/test-data/user.data';
 import { expect, test } from '@playwright/test';
 
 test.describe('Verify login', () => {
   test('login with correct credentials @GAD_R02_01', async ({ page }) => {
     //Arrange
-    const userEmail = 'wilma.langworth@test.test.dev';
+    const userEmail = testUser1.userEmail;
     const userPassword = '4dzFC';
     const loginPage = new LoginPage(page);
     await loginPage.goto();
