f86b81e7b96adda0c3534ef066b043fb415e76ca
PrzemysławPacyk <przemyslaw.pacyk@execon.pl>
2025-10-06 10:53:04 +0200

    chore: env for password

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -7,7 +7,7 @@ test.describe('Verify login', () => {
   test('login with correct credentials @GAD_R02_01', async ({ page }) => {
     //Arrange
     const userEmail = testUser1.userEmail;
-    const userPassword = '4dzFC';
+    const userPassword = testUser1.userPassword;
     const loginPage = new LoginPage(page);
     await loginPage.goto();
 
