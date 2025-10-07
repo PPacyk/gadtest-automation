b31953cdf35952c2383485b53de9cc311e2a57a6
PrzemysławPacyk <przemyslaw.pacyk@execon.pl>
2025-10-07 11:38:31 +0200

    test: negative for login

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -19,4 +19,22 @@ test.describe('Verify login', () => {
     //Assert
     expect(title).toContain('Welcome');
   });
+
+  test('reject login with incorrect password @GAD_R02_01', async ({ page }) => {
+    //Arrange
+    const userEmail = testUser1.userEmail;
+    const userPassword = 'incorrectPassword';
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+
+    //Act
+    await loginPage.login(userEmail, userPassword);
+
+    //Assert
+    await expect
+      .soft(loginPage.loginError)
+      .toHaveText('Invalid username or password');
+    const title = await loginPage.title();
+    expect.soft(title).toContain('Login');
+  });
 });
