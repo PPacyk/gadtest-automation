9e91435728df302bd0611a8d9a61f1434c198e02
PrzemysławPacyk <przemyslaw.pacyk@execon.pl>
2025-10-24 13:37:05 +0200

    chore: article model and factory

---
+++ src/views/add-article.view.ts
@@ -0,0 +1,22 @@
+import { AddArticleModel } from '../models/article.model';
+import { Locator, Page } from '@playwright/test';
+
+export class AddArticleView {
+  header: Locator;
+  titleInput: Locator;
+  bodyInput: Locator;
+  saveButton: Locator;
+
+  constructor(private page: Page) {
+    this.header = this.page.getByRole('heading', { name: 'Add New Entry' });
+    this.titleInput = this.page.getByTestId('title-input');
+    this.bodyInput = this.page.getByTestId('body-text');
+    this.saveButton = this.page.getByTestId('save');
+  }
+
+  async createArticle(addArticle: AddArticleModel): Promise<void> {
+    await this.titleInput.fill(addArticle.title);
+    await this.bodyInput.fill(addArticle.body);
+    await this.saveButton.click();
+  }
+}
