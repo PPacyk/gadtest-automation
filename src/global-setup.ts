61f8073ada301fbd773ad95fe5532c0e0f9d64b7
Przemyslaw Pacyk <paco1611@wp.pl>
2026-09-01 12:44:33 +0200

    chore: error handling for env variables

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -1,11 +1,4 @@
-import * as dotenv from 'dotenv';
-
-export const BASE_URL = process.env.BASE_URL ?? '[NOT SET]';
-export const USER_EMAIL = process.env.USER_EMAIL ?? '[NOT SET]';
-export const USER_PASSWORD = process.env.USER_PASSWORD ?? '[NOT SET]';
-
 async function globalSetup(): Promise<void> {
-  dotenv.config({ override: true });
   console.log('URL:', process.env.BASE_URL);
 }
 
