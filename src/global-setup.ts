de0bd08dc2a48985a2af3b14b6132b171ea08409
Przemyslaw Pacyk <paco1611@wp.pl>
2026-09-01 11:45:27 +0200

    chore: typescript errors corrected

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -1,4 +1,8 @@
-import dotenv from 'dotenv';
+import * as dotenv from 'dotenv';
+
+export const BASE_URL = process.env.BASE_URL ?? '[NOT SET]';
+export const USER_EMAIL = process.env.USER_EMAIL ?? '[NOT SET]';
+export const USER_PASSWORD = process.env.USER_PASSWORD ?? '[NOT SET]';
 
 async function globalSetup(): Promise<void> {
   dotenv.config({ override: true });
