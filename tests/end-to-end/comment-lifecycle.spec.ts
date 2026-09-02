2a77a2bd1619b3058b0874342e89e04d0685ff87
Przemyslaw Pacyk <paco1611@wp.pl>
2026-09-02 19:43:52 +0200

    wip: comment lifecycle, verify comment

--- tests/end-to-end/comment-lifecycle.spec.ts
+++ tests/end-to-end/comment-lifecycle.spec.ts
@@ -2,6 +2,7 @@ import { randomNewArticle } from '../../src/factories/article.factory';
 import { AddArticleModel } from '../../src/models/article.model';
 import { ArticlePage } from '../../src/pages/article.page';
 import { ArticlesPage } from '../../src/pages/articles.page';
+import { CommentPage } from '../../src/pages/comment.page';
 import { LoginPage } from '../../src/pages/login.page';
 import { testUser1 } from '../../src/test-data/user.data';
 import { AddArticleView } from '../../src/views/add-article.view';
@@ -15,6 +16,7 @@ test.describe('Create, verify and delete comment', () => {
   let articleData: AddArticleModel;
   let articlePage: ArticlePage;
   let commentView: AddCommentView;
+  let commentPage: CommentPage;
 
   test.beforeEach(async ({ page }) => {
     loginPage = new LoginPage(page);
@@ -22,6 +24,7 @@ test.describe('Create, verify and delete comment', () => {
     addArticleView = new AddArticleView(page);
     articlePage = new ArticlePage(page);
     commentView = new AddCommentView(page);
+    commentPage = new CommentPage(page);
 
     articleData = randomNewArticle();
 
@@ -32,21 +35,31 @@ test.describe('Create, verify and delete comment', () => {
     await addArticleView.createArticle(articleData);
   });
 
-  test('create new comment @GAD-R05-01', async () => {
+  test('create new comment @GAD-R05-01', async ({ page }) => {
     //Create new comment
     //Arrange
     const expectedAddCommentHeader = 'Add New Comment';
     const expectedCommentCreatedPopup = 'Comment was created';
+    const commentText = 'Hello mordo';
 
     //Act
     await articlePage.addCommentButton.click();
     await expect(commentView.header).toHaveText(expectedAddCommentHeader);
-    await commentView.bodyInput.fill('Hello mordo');
+    await commentView.bodyInput.fill(commentText);
     await commentView.saveButton.click();
 
     //Assert
     await expect(articlePage.articleErrorText).toHaveText(
       expectedCommentCreatedPopup,
     );
+
+    //Verify comment
+    //Act
+    const articleComment = articlePage.getArticleComment(commentText);
+    await expect(articleComment.body).toHaveText(commentText);
+    await articleComment.link.click();
+
+    //Assert
+    await expect(commentPage.commentBody).toHaveText(commentText);
   });
 });
